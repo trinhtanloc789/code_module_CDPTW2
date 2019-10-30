5ce7baa1c1bc310d931f77ce1995595f290c595f
trinhtanloc789 <trinhtanloc6589@gmail.com>
2019-10-30 11:29:24 +0700

    ok

--- 9/9.php
+++ 9/9.php
@@ -13,10 +13,7 @@ $url_path = str_replace('\\', '/', $url_path);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/all.css">
-    <script src="./js/all.js"></script>
     <link rel="stylesheet" href="./css/9.css">
     <title>Module 9</title>
 </head>
